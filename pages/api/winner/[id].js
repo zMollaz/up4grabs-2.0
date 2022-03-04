c8b3ee48298486977dadbe81af66c20797ba49f6
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:00:55 -0500

    Initial commit

---
+++ pages/api/winner/[id].js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+export default async function winnerHandler(req, res) {
+  if (req.method !== "GET" && req.method !== "POST") {
+    return res.status(405).json({ messsage: "Method not allowed" });
+  }
+  if (req.method === "GET") {
+    const winner = await prisma.Winners.findFirst({
+      where: {
+        listing_id: Number(req.query.id),
+      },
+    });
+    res.json({ winner });
+  }
+
+}
