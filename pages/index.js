787eaf857820c06875cc3e8bfadd1b787d2c5e74
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:00:59 -0500

    Initialize feature/date-picker

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Layout from "../components/Layout";
+import Header from "../components/Header";
+import Listings from "../components/Listings";
+import New from "../components/New";
+import prisma from "../lib/prisma";
+import {useState, useEffect} from 'react';
+
+
+export async function getStaticProps() {
+  const listings = await prisma.listings.findMany();
+  // console.log("inside fetcher", listings);
+  return {
+    props: { listings },
+  };
+}
+
+//use useEffect inside the component when you want to make additional queries to db or api like create
+
+export default function Home({ listings }) {
+  useEffect(() => { import('tw-elements'); }, []);
+  
+  const [display, setDisplay] = useState(false);
+  const handleClick = () => {
+    setDisplay((prev) => !prev);
+  };
+
+  return  (
+    <Layout handleClick={handleClick}>
+      <Header />
+      <Listings listings={listings}/>
+      {display && <New handleClick={handleClick} />}
+    </Layout>
+  );
+
+}
