b032bfb8089ea923501d075a8bd83b05f2f78ff0
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:01:04 -0500

    Initialize feature/styling-nav/modal

---
+++ components/PageBreak.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function PageBreak() {
+  return (
+
+      <div id="listings" className="mb-10 bg-off-white">
+        <div className="text-right"></div>
+        <div className="mt-10"></div>
+        <div >
+          <hr />
+        </div>
+        <div className="text-center text-gray-dark bg-off-white text-3xl font-extrabold p-4">
+          Listings
+        </div>
+        <div >
+          <hr />
+        </div>
+      </div>
+
+  );
+}
---
+++ pages/api/new.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import moment from "moment";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const uploadToWebApi = async (listing) => {
+  const image = listing.img_src;
+  const form = new URLSearchParams();
+  form.append("image", image);
+  const imageServerWebApi = `https://api.imgbb.com/1/upload?key=44cfa4dc7b5f14fa19e55b846de10cd4`;
+  const response = await axios.post(imageServerWebApi, form);
+  const retrievedUrl = response.data.data.display_url;
+  return retrievedUrl;
+};
+
+export default async function formHandler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ messsage: "Method not allowed" });
+  }
+  
+  const categoryToInteger = Number(req.body.category_id)
+  const imageUrl = await uploadToWebApi(req.body);
+  const startDate = moment().format("YYYY/MM/DD");
+  const newListing = {
+    ...req.body,
+    img_src: imageUrl,
+    user_id: 1,
+    category_id: categoryToInteger,
+    start_date: startDate,
+  };
+
+  const savedListing = await prisma.listings.create({
+    data: newListing,
+  });
+
+  res.json({ savedListing });
+}
