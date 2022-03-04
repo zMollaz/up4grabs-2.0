685a1ccc09a27469a575419d3d1c2323951d3ed9
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:01:14 -0500

    Initialize feature/fixing-modal

---
+++ pages/api/socket.js
@@ -0,0 +1,20 @@
+import { Server } from "Socket.IO";
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+    io.on("connection", (socket) => {
+      socket.on("sent", (msg) => {
+        console.log("Recieved at the back", msg)
+        socket.broadcast.emit("return", msg);
+      });
+    });
+  }
+  res.end();
+};
+
+export default SocketHandler;
