063f5200d376094f5408c011dae23249ae5a085b
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:00:56 -0500

    Initialize feature/header

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-10 w-auto min-w-full max-h-3/6 max-w-none"
+      >
+        <source src="/videos/header2.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </header>
+  );
+}
