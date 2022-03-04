376679c5b68ec7c6352645db8f349f437eb9889f
Ahmed Elmolla <87554450+zMollaz@users.noreply.github.com>
2022-03-04 08:01:22 -0500

    Initialize feature/footer-responsive

---
+++ components/Countdown.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function Countdown({
+  end_date,
+  biddings,
+  user,
+  users,
+  listingItem,
+  timeUp,
+  setTimeUp,
+  winner,
+  setWinner
+}) {
+
+ 
+  const timeRemaining = new Date(end_date) - new Date();
+  const getCountdown = () => {
+    const year = new Date().getFullYear() + 1;
+    let countdown = {};
+    if (timeRemaining > 0) {
+      countdown = {
+        Days: Math.floor(timeRemaining / (1000 * 60 * 60 * 24)),
+        Hours: Math.floor((timeRemaining / (1000 * 60 * 60)) % 24),
+        Minutes: Math.floor((timeRemaining / 1000 / 60) % 60),
+        Seconds: Math.floor((timeRemaining / 1000) % 60),
+      };
+    }
+    return countdown;
+  };
+  const [countdown, setCountdown] = useState(getCountdown());
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setCountdown(getCountdown());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const data = [];
+  Object.entries(countdown).forEach(([unit, value]) => {
+    data.push(
+      <span key={Math.random().toString(16)}>
+        <strong>{value}</strong> {unit}
+      </span>
+    );
+  });
+
+  const randomWinner = (biddings, users, listing) => {
+    const bidders = biddings.map((bidding) => bidding.user_id);
+    const winnerId = bidders[Math.floor(Math.random() * bidders.length)];
+    const itemWinner = users.find((user) => user.id === winnerId);
+    const data = {
+      winner: itemWinner,
+      listingTitle: listing.title,
+      listingImage: listing.img_src,
+    };
+
+    if (itemWinner) {
+      axios
+        .post("/api/email", data)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
+      return itemWinner;
+    }
+  };
+
+
+  useEffect(() => {
+    if (timeUp) {
+
+      setWinner(randomWinner(biddings, users, listingItem));
+    }
+    // if (timeRemaining <= 0)
+    // // setWinner(randomWinner(biddings, users, listingItem));
+    // // setTimeUp(prev => !prev)
+    // console.log(333, timeRemaining)
+    //might need clean up because of memory leak
+  }, [timeUp]);
+
+  // const timeUp = true;
+
+  return (
+    <>
+      {!timeUp ? (
+        <div className="py-2 border-gray-200 text-red text-xl grid grid-flow-col gap-2 text-center auto-cols-max">
+          {data} until draw!
+        </div>
+      ) : (
+        <div className="py-2 border-gray-200 font-bold text-green text-xl grid grid-flow-col gap-2 text-center auto-cols-max">
+          Winner is {winner?.name}
+        </div>
+      )}
+    </>
+  );
+}
---
+++ hooks/useUsers.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import cookie from "cookie-cutter";
+
+const defaultUser = { id: 1 };
+
+const useUsers = () => {
+  
+  const [loaded, setLoaded] = useState(false);
+  const [user, setUser] = useState(defaultUser);
+  const [users, setUsers] = useState([]);
+
+  const switchUser = (id) => {
+
+    let uId = id;
+    if (!uId) {
+      uId = 1;
+    }
+      cookie.set("id", uId, { path: "/" });
+      const user = users.find((user) => user.id === Number(uId));
+      setUser(user || defaultUser);
+
+  };
+
+  useEffect(() => {
+    getUsers()
+    .then((response) => {
+      setUsers(response.data.users);
+  
+      setLoaded(true)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, [user.id]);
+  
+  useEffect(() => {
+    const cookieId = Number(cookie.get("id")) || 1;
+    switchUser(cookieId);
+  }, [loaded]);
+
+  const getUsers = async () => {
+    const users = axios.get("/api/users");
+    
+    return users
+  };
+
+  return { user, users, switchUser, loaded }; // this is what's in the context app.js
+};
+
+export default useUsers;
