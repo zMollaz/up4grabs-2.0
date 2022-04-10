da5bae85926f6c263f0f29980c9dbbb0fd0330c9
A E <aelmollaz@gmail.com>
2022-04-10 10:01:07 -0400

    Create functional draft login page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,8 @@ import NextAuth from "next-auth";
 // import EmailProvider from "next-auth/providers/email";
 import CredentialsProvider from "next-auth/providers/credentials";
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+
+/* Default login page
 
 export default NextAuth({
   providers: [
@@ -60,5 +61,61 @@ export default NextAuth({
       },
     }),
   ],
+  secret: process.env.JWT_SECRET,
+});
+*/
+
+//Custom login page
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "enter your password",
+        },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        console.log(111, credentials)
+        const dbUser = await prisma.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+        console.log(222, dbUser)
+        const user = { id: dbUser.id, name: dbUser.name, email: dbUser.email };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   secret: process.env.JWT_SECRET,
 });
