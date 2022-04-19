6db421f785bb30f08dc01284d0b48f07972dbf57
A E <aelmollaz@gmail.com>
2022-04-18 23:27:31 -0400

    Fix new modal positioning in xs screens

--- components/New.jsx
+++ components/New.jsx
@@ -60,42 +60,25 @@ export default function New({ handleClick, setDisplay }) {
   return (
     <div
       aria-hidden="true"
-      className={`overflow-y-auto overflow-x-auto fixed left-0 top-0 z-50 justify-center items-center w-full h-full inset-0`}
+      className={`overflow-y-auto overflow-x-auto fixed  z-50 justify-center items-center w-full h-full inset-0`}
     >
-      <div className="absolute inset-0  w-full  h-full">
+      <div className="w-full  h-full">
         <div
           onClick={handleClick}
-          className="flex w-full h-full absolute opacity-75 bg-t-gray"
+          className="flex w-full h-full opacity-75 bg-t-gray"
         >
-          {/* <button
-            onClick={handleClick}
-            type="button"
-            className="text-black hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center  absolute right-0 top-0"
-          >
-            <svg
-              className="w-7 h-7 bg-white rounded-lg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button> */}
+
         </div>
         <form
           onSubmit={saveListing}
           // className="center w-5/12 overflow-auto px-4 items-center pb-4 space-y-8 rounded-lg lg:px-8 sm:pb-6  bg-white fixed inset-24 "
-          className="transform -translate-x-1/2 -translate-y-1/2 h-fit w-fit overflow-auto px-4 items-center pb-4 space-y-8 rounded-lg lg:px-8 sm:pb-6  bg-white  absolute top-[50%] left-[50%]"
+          className="transform -translate-x-1/2 -translate-y-1/2 xs:h-[80%] xs:w-[80%] overflow-auto px-4 items-center pb-4 space-y-8 rounded-lg lg:px-8 sm:pb-6  bg-white  absolute top-[50%] left-[50%]"
           action="#"
         >
           <button
             onClick={handleClick}
             type="button"
-            className="text-black hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center  absolute right-0 top-0"
+            className="text-black hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center absolute right-0 top-0"
           >
             <svg
               className="w-7 h-7 bg-gray-dark rounded-lg"
